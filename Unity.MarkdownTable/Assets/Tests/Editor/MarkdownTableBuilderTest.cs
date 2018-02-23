1fcf2935ab6dbfe4a3c56434f4c191d0003d0320
Marco Link <marcoxlink@googlemail.com>
2018-02-23 18:19:34 +0100

    Add gradle and paket support

---
+++ Unity.MarkdownTable/Assets/Tests/Editor/MarkdownTableBuilderTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+using NUnit.Framework;
+
+namespace Tests.Editor
+{
+    public class MarkdownTableBuilderTest
+    {
+        private StringBuilder builder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            builder = new StringBuilder();
+        }
+    }
+}
